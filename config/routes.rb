c0aed04e0e6f30f3f94a05bec53d0dd1be31869d
kokewater <ryokokeguchi@gmail.com>
2020-12-07 09:56:55 +0000

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+  namespace :admin do
+    get 'top' => 'homes#top'
+    resources :users, only: [:index, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+    resources :questions, only: [:index, :show, :destroy] do
+      resources :answers, only: [:destroy]
+    end
+  end
+
+
+  devise_for :users, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+
+  scope module: :public do
+    root 'homes#top'
+    get "about" => "homes#about"
+    resources :users, only: [:index, :show, :edit, :update] do
+      resources :relationships, only: [:create, :destroy]
+      member do
+        get :quit
+        get :out
+      end
+    end
+    resources :questions do
+      collection do
+        get :sort
+        get :search
+      end
+      resources :question_likes, only: [:create, :destroy]
+      resources :answers, only: [:create, :destroy]
+    end
+
+
+    resources :answer_likes, only: [:create, :destroy]
+    resources :post_messages, only: [:index, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
