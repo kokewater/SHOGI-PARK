23848ac19487aa889e06c731e47e768e8d4d1542
kokewater <ryokokeguchi@gmail.com>
2020-12-10 08:07:45 +0000

    投稿編集削除機能

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,10 +1,10 @@
 class Public::QuestionsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
     @question = Question.new
   end
-  
+
   def create
     @question = Question.new(question_params)
     @question.user_id = current_user.id
@@ -14,38 +14,51 @@ class Public::QuestionsController < ApplicationController
       render :new
     end
   end
-  
+
   def index
     @questions = Question.all.includes(:user)
     @genres = Genre.all
   end
-  
+
   def sort
     @questions = Question.all.includes(:user)
     @genres = Genre.all
     @genre = Genre.find(params[:genre_id])
   end
-  
+
   def show
     @question = Question.find(params[:id])
     @genres = Genre.all
   end
-  
+
   def edit
+    @question = Question.find(params[:id])
+    if @question.user_id != current_user.id
+      redirect_to questions_path
+    end
   end
-  
+
   def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to question_path(@question), notice: "編集しました"
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
-  
+
   def search
   end
-  
+
   private
     def question_params
       params.require(:question).permit(:genre_id, :title, :body, :image)
     end
-  
+
 end
---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,19 @@
+class Public::UsersController < ApplicationController
+  def show
+  end
+  
+  def index
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def quit
+  end
+  
+  def out
+    
+end
