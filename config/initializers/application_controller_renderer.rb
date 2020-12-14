8905acd7ae09e35c37d3a2b4a615c726834f3690
kokewater <ryokokeguchi@gmail.com>
2020-12-14 08:32:56 +0000

    エラー解消

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8069891f25ce03e87889ad837543904e3120149b7a4f4be9b9d0ea26343dc1e51e7ed0e84edea8aca6078057703d8e3a7cadad516837ec790511f22c60b864ad'
