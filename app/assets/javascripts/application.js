60c0a63fbbf62106f9d4ae132433ee3e7e0eaf6e
kokewater <ryokokeguchi@gmail.com>
2020-12-17 07:49:40 +0000

    ハンバーガーメニューレスポンシブ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+// トップページスライダー
 $(document).ready(function () {
   $("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
@@ -39,5 +40,16 @@ $(document).ready(function () {
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
+  });
+});
+
+// ハンバーガーメニュー
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    $('.menu-trigger').on('click', function(event) {
+      $(this).toggleClass('active');
+      $('#sp-menu').fadeToggle();
+      event.preventDefault();
+    });
   });
 });
