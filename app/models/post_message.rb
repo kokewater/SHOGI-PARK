2856776658ebe85b4d6257739c357ffef20294eb
kokewater <ryokokeguchi@gmail.com>
2020-12-21 03:52:06 +0000

    aboutページ、掲示板投稿制限

--- app/models/post_message.rb
+++ app/models/post_message.rb
@@ -2,4 +2,8 @@ class PostMessage < ApplicationRecord
   belongs_to :user
 
   validates :message, presence: true, length: {maximum: 300}
+
+  def obey_1min_role?(user)
+    user.post_messages.last(5).first.created_at <= 1.minutes.ago
+  end
 end
