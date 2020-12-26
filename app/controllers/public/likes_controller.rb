e1d9388d5b22917d3d207b534cbb574a94c2d5ef
kokewater <ryokokeguchi@gmail.com>
2020-12-26 02:55:34 +0000

    いいね非同期不具合修正

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -3,16 +3,14 @@ class Public::LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    question = Question.find(params[:question_id])
-    like = current_user.likes.new(question_id: question.id)
+    @question = Question.find(params[:question_id])
+    like = current_user.likes.new(question_id: @question.id)
     like.save
-    redirect_to request.referer
   end
 
   def destroy
-    question = Question.find(params[:question_id])
-    like = current_user.likes.find_by(question_id: question.id)
+    @question = Question.find(params[:question_id])
+    like = current_user.likes.find_by(question_id: @question.id)
     like.destroy
-    redirect_to request.referer
   end
 end
